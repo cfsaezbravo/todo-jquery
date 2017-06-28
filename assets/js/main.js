0721da0e0ced8e1bf8d081ab04007586f8215e83
Camila Sáez <cfsaezbravo@gmail.com>
2017-06-28 17:32:03 -0400

    Deleting items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
 	$(".btn").click(function(e){
 		e.preventDefault();
+
 		var tareaNueva = $("#tareaNueva").val();
+
 		if(tareaNueva == ""){
 			alert("Debes escribir una tarea");
 		}else{
-			$("#listaTareas").append("<li>" + tareaNueva);
-		}
+			$("#listaTareas").append("<li><input type=checkbox>" + "<span id='tareaAgregada'>" + tareaNueva + "</span>" + "<a class='waves-effect waves-light btn pink' id='btn-eliminar'><i class='material-icons right'>delete</i></a>");
+			$("#tareaNueva").val("");
+
+			$("#btn-eliminar").click(function(){
+				$(this).parent().remove();
+				this.remove();
+			})
+		}	
+
+		
 	});
+
+	
 });
